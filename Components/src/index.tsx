a8b97b568a19be21bbe0782043f59fcbf13abbee
KononOleg <konon.oleg97@gmail.com>
2021-07-29 16:21:05 +0300

    feat: add header

--- Components/src/index.tsx
+++ Components/src/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { MainPage } from "./pages/MainPage/MainPage";
 
 window.onload = () => {
+  document.body.classList.add("body");
 
+  ReactDOM.render(<MainPage />, document.body);
 };
---
+++ Components/src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Header } from "../../components/header/Header";
+
+export function MainPage(): JSX.Element {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
