091e44e9ae19dfaba0ad28643f73cc1dc8692a52
KononOleg <konon.oleg97@gmail.com>
2021-08-03 20:35:50 +0300

    fix: validation errors

--- Forms/src/components/Form/Form.tsx
+++ Forms/src/components/Form/Form.tsx
@@ -2,16 +2,18 @@ import './Form.scss';
 import React, { useEffect, useState } from 'react';
 import { Switch } from '../Switch/Switch.tsx';
 
+const SELECT_DEFAULT = 'DEFAULT';
+
 export function Form(): JSX.Element {
   const [textInput, setTextInput] = useState('');
   const [dateInput, setDateInput] = useState('');
-  const [selectInput, setSelectInput] = useState('');
+  const [selectInput, setSelectInput] = useState(SELECT_DEFAULT);
   const [checkboxInput, setCheckboxInput] = useState(false);
   const [switchInput, setSwitchInput] = useState(false);
 
   const [isFormValid, setIsFormValid] = useState(false);
 
-  
+  const [dateInputType, setDateInputType] = useState('text');
 
   const [textInputError, setTextInputError] = useState('');
   const [dateInputError, setDateInputError] = useState('');
@@ -22,11 +24,13 @@ export function Form(): JSX.Element {
   }, [checkboxInput]);
 
   const textInputHandler = (e: React.FormEvent<HTMLInputElement>) => {
-    if (!e.currentTarget.value) setTextInputError('не может быть пустым');
-    else {
+    if (!e.currentTarget.value) setTextInputError('Name cannot be blank');
+    else if (!/^[^(~ ! @ # $ % * () _ — + = | : ; " ' ` < > , . ? / ^0-9)]*$/.test(e.currentTarget.value)) {
+      setTextInputError('Name cannot contain digits');
+    } else {
       setTextInputError('');
-      setTextInput(e.currentTarget.value);
     }
+    setTextInput(e.currentTarget.value);
   };
 
   const dateInputHandler = (e: React.FormEvent<HTMLInputElement>) => {
@@ -42,6 +46,17 @@ export function Form(): JSX.Element {
   };
   const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (!textInput) setTextInputError('Name cannot be blank');
+    if (selectInput === SELECT_DEFAULT) setSelectInputError('Country cannot be blank');
+    if (!dateInput) setDateInputError('Date cannot be blank');
+    if (!textInputError && !dateInputError && !selectInputError && checkboxInput) {
+      setTextInput('');
+      setDateInput('');
+      setSelectInput(SELECT_DEFAULT);
+      setSwitchInput(false);
+      setCheckboxInput(false);
+      setDateInputType('text');
+    }
   };
   const switchInputHandler = () => {
     setSwitchInput(!switchInput);
@@ -50,32 +65,39 @@ export function Form(): JSX.Element {
   return (
     <form className="form" onSubmit={submitHandler}>
       <label className="label__error">
-        <input type="text" className="input" placeholder="What is your name?" onChange={textInputHandler} />
+        <input type="text" className={`input ${textInputError && 'input_error'}`} placeholder="What is your name?" onChange={textInputHandler} value={textInput} />
         {textInputError && textInputError}
       </label>
       <label className="label__error">
-        <input type="date" className="input input_date" data-placeholder="Bastille day" required aria-required="true" onChange={dateInputHandler} />
+        <input
+          type={dateInputType}
+          className={`input ${dateInputError && 'input_error'}`}
+          onChange={dateInputHandler}
+          value={dateInput}
+          placeholder="Bastille day"
+          onMouseDown={() => setDateInputType('date')}
+        />
         {dateInputError && dateInputError}
       </label>
       <label className="label__error">
-      <select defaultValue={'DEFAULT'} className="input" onChange={selectInputHandler}>
-        <option value="DEFAULT" disabled>
-          Choose your country
-        </option>
-        <option value="Kazakhstan">Kazakhstan</option>
-        <option value="Ukraine">Ukraine</option>
-        <option value="Mordor">Mordor</option>
-        <option value="Pandora">Pandora</option>
-      </select>
-      {selectInputError && selectInputError}
+        <select value={selectInput} className={`input ${selectInputError && 'input_error'}`} onChange={selectInputHandler}>
+          <option value={SELECT_DEFAULT} disabled>
+            Choose your country
+          </option>
+          <option value="Kazakhstan">Kazakhstan</option>
+          <option value="Ukraine">Ukraine</option>
+          <option value="Mordor">Mordor</option>
+          <option value="Pandora">Pandora</option>
+        </select>
+        {selectInputError && selectInputError}
       </label>
       <div className="switch__wrapper">
-        <Switch switchHandler={switchInputHandler} />
-        <p className="switch__text">Are you hungry?</p>
+        <Switch switchHandler={switchInputHandler} isChecked={switchInput} />
+        <p className="form__text">Are you hungry?</p>
       </div>
 
       <label className="label">
-        <input type="checkbox" name="happy" value="yes" onChange={checkboxInputHandler} />I agree with chipping
+        <input type="checkbox" onChange={checkboxInputHandler} checked={checkboxInput} />I agree with chipping
       </label>
       <button type="submit" className="button_submit" disabled={!!isFormValid}>
         Submit
--- Forms/src/components/Switch/Switch.tsx
+++ Forms/src/components/Switch/Switch.tsx
@@ -2,13 +2,14 @@ import './Switch.scss';
 import React from 'react';
 
 interface IProps {
+  isChecked: boolean;
   switchHandler: () => void;
 }
 
 export function Switch(props: IProps): JSX.Element {
   return (
     <label className="switch">
-      <input type="checkbox" onChange={props.switchHandler}></input>
+      <input type="checkbox" onChange={props.switchHandler} checked={props.isChecked}></input>
       <span className="slider"></span>
       <span className="switch__span span__train">no</span>
       <span className="switch__span span__play">yes</span>
