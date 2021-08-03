ab5c57acd276acee671e7f100c796bc52a3c1183
KononOleg <konon.oleg97@gmail.com>
2021-08-03 16:48:25 +0300

    feat: add  validation

--- Forms/src/components/Form/Form.tsx
+++ Forms/src/components/Form/Form.tsx
@@ -1,34 +1,83 @@
 import './Form.scss';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Switch } from '../Switch/Switch.tsx';
 
 export function Form(): JSX.Element {
+  const [textInput, setTextInput] = useState('');
+  const [dateInput, setDateInput] = useState('');
+  const [selectInput, setSelectInput] = useState('');
+  const [checkboxInput, setCheckboxInput] = useState(false);
+  const [switchInput, setSwitchInput] = useState(false);
+
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  
+
+  const [textInputError, setTextInputError] = useState('');
+  const [dateInputError, setDateInputError] = useState('');
+  const [selectInputError, setSelectInputError] = useState('');
+
+  useEffect(() => {
+    setIsFormValid(!isFormValid);
+  }, [checkboxInput]);
+
+  const textInputHandler = (e: React.FormEvent<HTMLInputElement>) => {
+    if (!e.currentTarget.value) setTextInputError('не может быть пустым');
+    else {
+      setTextInputError('');
+      setTextInput(e.currentTarget.value);
+    }
+  };
+
+  const dateInputHandler = (e: React.FormEvent<HTMLInputElement>) => {
+    if (e.currentTarget.value) setDateInputError('');
+    setDateInput(e.currentTarget.value);
+  };
+  const checkboxInputHandler = () => {
+    setCheckboxInput(!checkboxInput);
+  };
+  const selectInputHandler = (e: React.FormEvent<HTMLSelectElement>) => {
+    if (e.currentTarget.value) setSelectInputError('');
+    setSelectInput(e.currentTarget.value);
+  };
   const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+  };
+  const switchInputHandler = () => {
+    setSwitchInput(!switchInput);
   };
 
   return (
     <form className="form" onSubmit={submitHandler}>
-      <input type="text" className="input" placeholder="What is your name?" />
-      <input type="date" className="input input_date" data-placeholder="Bastille day" required aria-required="true" />
-      <select defaultValue={'DEFAULT'} className="input">
+      <label className="label__error">
+        <input type="text" className="input" placeholder="What is your name?" onChange={textInputHandler} />
+        {textInputError && textInputError}
+      </label>
+      <label className="label__error">
+        <input type="date" className="input input_date" data-placeholder="Bastille day" required aria-required="true" onChange={dateInputHandler} />
+        {dateInputError && dateInputError}
+      </label>
+      <label className="label__error">
+      <select defaultValue={'DEFAULT'} className="input" onChange={selectInputHandler}>
         <option value="DEFAULT" disabled>
           Choose your country
         </option>
-        <option value="value2">Kazakhstan</option>
-        <option value="value3">Ukraine</option>
-        <option value="value3">Mordor</option>
-        <option value="value3">Pandora</option>
+        <option value="Kazakhstan">Kazakhstan</option>
+        <option value="Ukraine">Ukraine</option>
+        <option value="Mordor">Mordor</option>
+        <option value="Pandora">Pandora</option>
       </select>
+      {selectInputError && selectInputError}
+      </label>
       <div className="switch__wrapper">
-        <Switch />
+        <Switch switchHandler={switchInputHandler} />
         <p className="switch__text">Are you hungry?</p>
       </div>
 
       <label className="label">
-        <input type="checkbox" name="happy" value="yes" />I agree with chipping
+        <input type="checkbox" name="happy" value="yes" onChange={checkboxInputHandler} />I agree with chipping
       </label>
-      <button type="submit" className="button_submit">
+      <button type="submit" className="button_submit" disabled={!!isFormValid}>
         Submit
       </button>
     </form>
--- Forms/src/components/Switch/Switch.tsx
+++ Forms/src/components/Switch/Switch.tsx
@@ -1,10 +1,14 @@
 import './Switch.scss';
 import React from 'react';
 
-export function Switch(): JSX.Element {
+interface IProps {
+  switchHandler: () => void;
+}
+
+export function Switch(props: IProps): JSX.Element {
   return (
     <label className="switch">
-      <input type="checkbox"></input>
+      <input type="checkbox" onChange={props.switchHandler}></input>
       <span className="slider"></span>
       <span className="switch__span span__train">no</span>
       <span className="switch__span span__play">yes</span>
